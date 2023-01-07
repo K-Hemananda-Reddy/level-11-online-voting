3409d861d1566bbb29aca239b26cad1e26a10976
Asaduddin aman <nithish.2302@gmail.com>
2023-01-07 15:06:45 +0530

    added all models

--- models/model_questions.js
+++ models/model_questions.js
@@ -9,12 +9,78 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static async getNumberOfQuestionss(election_id) {
+      return await this.count({
+        where: {
+          election_id,
+        },
+      });
+    }
+
+    static updateAQuestion({ question_name, question_description, id }) {
+      return this.update(
+        {
+          question_name,
+          question_description,
+        },
+        {
+          returning: true,
+          where: {
+            id,
+          },
+        }
+      );
+    }
+
+    static addAQuestion({ question_name, question_description, election_id }) {
+      return this.create({
+        question_name,
+        question_description,
+        election_id,
+      });
+    }
+
+    static async getQuestion(id) {
+      return await this.findOne({
+        where: {
+          id,
+        },
+      });
+    }
+
+    static deleteAQuestion(id) {
+      return this.destroy({
+        where: {
+          id,
+        },
+      });
+    }
+
+    static async getQuestionss(election_id) {
+      return await this.findAll({
+        where: {
+          election_id,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
     static associate(models) {
       // define association here
+      questionsModel.belongsTo(models.model_election, {
+        foreignKey: "election_id",
+      });
+
+      questionsModel.hasMany(models.model_option, {
+        foreignKey: "question_id",
+      });
     }
   }
   model_questions.init({
-    question_name: DataTypes.STRING,
+    question_name:  {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     question_description: DataTypes.STRING
   }, {
     sequelize,
--- models/model_voter.js
+++ models/model_voter.js
@@ -4,20 +4,78 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class model_voter extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    resetPassword(password) {
+      return this.update({ password });
+    }
+
+    static async createAVoter({ voterid, password, electionID }) {
+      return await this.create({
+        voterid,
+        password,
+        electionID,
+        votedOrNot: false,
+      });
+    }
+
+    static async getNumberOfVoterss(electionID) {
+      return await this.count({
+        where: {
+          electionID,
+        },
+      });
+    }
+
+    static async gettVoters(electionID) {
+      return await this.findAll({
+        where: {
+          electionID,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async getVoter(id) {
+      return await this.findOne({
+        where: {
+          id,
+        },
+      });
+    }
+
+    static async deleteAVoter(id) {
+      return await this.destroy({
+        where: {
+          id,
+        },
+      });
+    }
+
     static associate(models) {
       // define association here
+      voterModel.belongsTo(models.electionModel, {
+        foreignKey: "electionID",
+      });
     }
   }
   model_voter.init({
-    voter_id: DataTypes.STRING,
-    password: DataTypes.STRING,
-    voted: DataTypes.STRING,
-    role: DataTypes.STRING
+    voter_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password:  {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    voted:  {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: "voter",
+    }
   }, {
     sequelize,
     modelName: 'model_voter',
